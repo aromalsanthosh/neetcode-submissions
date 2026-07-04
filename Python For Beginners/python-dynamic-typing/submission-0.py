2d89aed1dd743b15dabf9a0b49a2404fa88f233e
Aromal S <aromalsanthosh@protonmail.com>
2026-07-04 22:15:05 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.6
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "lekshmi"
+print(type(variable))
+variable = ["IBM", "FAANG"]
+print(type(variable))
